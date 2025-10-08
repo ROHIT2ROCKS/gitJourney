e6f3bf4c3150c88da7a405753b07a8fdbff1a5cb
Rohit Lakhani <rohitlakhani1999@gmail.com>
2025-10-08 14:30:23 +0530

    added function display_vector()

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void display_vector(vector<int> arr) {
+    for (auto i : arr) {
+        cout << i <<" ";
+    }
+    cout << endl;
+}
+
 int main() {
-    cout << "Hello world";
+    vector<int> arr = {1,2,3,4,5,6,7,8,9};
+    display_vector(arr);
     return 1;
 }
